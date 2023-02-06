9ed23529363bf44df61251b6feed4ecab5d408f3
dagrewal <3958743+dagrewal@users.noreply.github.com>
2023-02-05 21:25:20 -0500

    added ADF testing

--- ch2.py
+++ ch2.py
@@ -1,5 +1,6 @@
 import quandl
 import yfinance as yf
+from fredapi import Fred
 import numpy as np
 import pandas as pd
 import datetime
@@ -15,8 +16,15 @@ ticker = '^GSPC'
 start = datetime.datetime(2015, 1, 1)
 end = datetime.datetime(2021, 1, 1)
 SP_prices = yf.download(ticker, start=start, end=end, interval='1mo').Close
+stat_test = adfuller(SP_prices)[0:2]
+diff_SP_price = SP_prices.diff()
+stat_test_2 = adfuller(diff_SP_price.dropna())[0:2]
 
 oil = quandl.get("NSE/OIL", authtoken="XTF1wfLZtp4bknFrzpmN", start_date="1980-01-01", end_date="2020-01-01")
+
+#fred = Fred(api_key='d2fa12e7a0ddb0a857b93bdf0c58dac6')
+
+#energy = fred.get_series("CAPUTLG2211A2S", observation_start="2010-01-01", observation_end="2020-12-31")
 
 if __name__ == "__main__":
     """
@@ -30,6 +38,28 @@ if __name__ == "__main__":
     #seasonal_decompose(SP_prices, period=12).plot()
     #plt.show()
 
-    sm.graphics.tsa.plot_acf(SP_prices, lags=30)
-    plt.xlabel('Number of Lags')
-    plt.show
+    #sm.graphics.tsa.plot_acf(SP_prices, lags=30)
+    #sm.graphics.tsa.plot_pacf(SP_prices, lags=30)
+
+    #energy.head()
+
+    #plt.plot(energy)
+    #plt.title('Energy Capacity Utilization')
+    #plt.ylabel('$')
+    #plt.xlabel('Date')
+    #plt.show()
+
+    print("The test statistic and p-value of ADF test are {}".format(stat_test))
+    plt.figure(figsize=(10,6))
+    plt.plot(diff_SP_price)
+    plt.title("Differenced S&P 500 Price")
+    plt.ylabel('$')
+    plt.xlabel('Date')
+    plt.show()
+    sm.graphics.tsa.plot_acf(diff_SP_price.dropna(), lags=30)
+    plt.xlabel("Number of Lags")
+    plt.show()
+    print("The test statistic and p-value of ADF test after differencing are {}".format(stat_test_2))
+    #sm.graphics.tsa.plot_acf(energy, lags=30)
+    #plt.xlabel('Number of Lags')
+    #plt.show()
